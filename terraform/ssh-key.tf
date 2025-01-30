fdf2cf3e7f0d75af2cc64cbddad438b2b03c5dd8
Bertrand Cachet <bertrand.cachet@gmail.com>
2025-01-30 12:24:44 +0000

    do not output SSH key

--- terraform/ssh-key.tf
+++ terraform/ssh-key.tf
@@ -8,10 +8,10 @@ resource "exoscale_ssh_key" "deployment_ssh_key" {
   public_key = tls_private_key.deployment_ssh_key.public_key_openssh
 }
 
-# Output private key to a file that will be used to SSH to created instances
-# Another approach can be to extract public key from a private key that has been provided
-resource "local_file" "deployment_ssh_key" {
-  content         = tls_private_key.deployment_ssh_key.private_key_pem
-  filename        = "../config/private_key.pem"
-  file_permission = "0400"
-}
+# # Output private key to a file that will be used to SSH to created instances
+# # Another approach can be to extract public key from a private key that has been provided
+# resource "local_file" "deployment_ssh_key" {
+#   content         = tls_private_key.deployment_ssh_key.private_key_pem
+#   filename        = "../config/private_key.pem"
+#   file_permission = "0400"
+# }
