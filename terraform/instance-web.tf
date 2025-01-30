d5d5b9c7aa8b2768ebd21b224d3f54c32b69ba3f
Bertrand Cachet <bertrand.cachet@gmail.com>
2025-01-30 10:56:32 +0000

    Add worker

--- terraform/instance-web.tf
+++ terraform/instance-web.tf
@@ -7,7 +7,6 @@ resource "exoscale_instance_pool" "web-ipool" {
   disk_size          = 10
   key_pair           = exoscale_ssh_key.deployment_ssh_key.name
   security_group_ids = [exoscale_security_group.web.id]
-  user_data          = file("./cloud-init.yml")
 }
 
 resource "exoscale_nlb" "web-nlb" {
